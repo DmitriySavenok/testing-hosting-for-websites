54245d258e9e1205f19fd6054cb334032f3621fb
Савенок Дмитрий <36520766+DmitriySavenok@users.noreply.github.com>
2021-09-16 00:01:47 +0300

    Тест сайта фонда

--- js/script.js
+++ js/script.js
@@ -98,3 +98,26 @@ $(function() {
     });
     
    });
+
+
+// Contact us (send message script)
+$(document).ready(function() {
+
+	//E-mail Ajax Send
+	$("form").submit(function() { //Change
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "../mail.php", //Change
+			data: th.serialize()
+		}).done(function() {
+			alert("Ваше сообщение отправлено. Спасибо!");
+			setTimeout(function() {
+				// Done Functions
+				th.trigger("reset");
+			}, 1000);
+		});
+		return false;
+	});
+
+});
