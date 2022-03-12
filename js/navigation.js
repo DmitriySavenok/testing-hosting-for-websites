2e52a87a028ad4e01ff2a35f095245b0c83b9cba
Савенок Дмитрий <36520766+DmitriySavenok@users.noreply.github.com>
2022-03-13 00:15:04 +0300

    тест анимации

--- js/navigation.js
+++ js/navigation.js
@@ -1,26 +1,56 @@
-function navigationToggle() {
-  var logo = document.getElementById("logo");
+var logo = document.getElementById("logo");
   var navigation = document.getElementById("navigation");
   var buttonOpen = document.getElementById("navigation-button-open");
   var buttonClose = document.getElementById("navigation-button-close");
   var navigatioList = document.querySelector(".navigation__list");
+
+
+function navigationToggle() {
   var navigationIsClose = navigation.classList.contains("navigation-closed");
 
   if (navigationIsClose == true) {
     navigation.classList.remove("navigation-closed")
     navigation.classList.add("navigation__opened")
-    buttonOpen.classList.add("navigation-button-hide")
-    buttonClose.classList.remove("navigation-button-hide")
-    navigatioList.classList.remove("navigation-button-hide")
+    buttonOpen.classList.add("visually-hidden")
+    buttonClose.classList.remove("visually-hidden")
+    navigatioList.classList.remove("visually-hidden")
     logo.classList.add("logo-fixed")
   }
 
   if (navigationIsClose == false) {
     navigation.classList.add("navigation-closed")
     navigation.classList.remove("navigation__opened")
-    buttonOpen.classList.remove("navigation-button-hide")
-    buttonClose.classList.add("navigation-button-hide")
-    navigatioList.classList.add("navigation-button-hide")
+    buttonOpen.classList.remove("visually-hidden")
+    buttonClose.classList.add("visually-hidden")
+    navigatioList.classList.add("visually-hidden")
     logo.classList.remove("logo-fixed")
   }
 }
+
+document.querySelectorAll('a[href^="#"').forEach(link => {
+
+  link.addEventListener('click', function(e) {
+      e.preventDefault();
+
+      let href = this.getAttribute('href').substring(1);
+
+      const scrollTarget = document.getElementById(href);
+
+      const topOffset = document.querySelector('.scrollto').offsetHeight;
+      // const topOffset = 0; // если не нужен отступ сверху
+      const elementPosition = scrollTarget.getBoundingClientRect().top;
+      const offsetPosition = elementPosition - topOffset;
+
+      navigation.classList.add("navigation-closed")
+      navigation.classList.remove("navigation__opened")
+      buttonOpen.classList.remove("visually-hidden")
+      buttonClose.classList.add("visually-hidden")
+      navigatioList.classList.add("visually-hidden")
+      logo.classList.remove("logo-fixed")
+
+      window.scrollBy({
+          top: offsetPosition,
+          behavior: 'smooth'
+      });
+  });
+});
--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
+let isScroll = 0, // доп. проверка
+    targetScroll = 10; // расстояние до действия / в px
 
+$(window).on('scroll', function(){
+  if(isScroll === 0 && $(this).scrollTop() >= targetScroll) {
+    isScroll = 1;
+    $('.photo-main').removeClass('photo-position-from')
+    $('.photo-main').addClass('photo-position-to');
+    console.info('change 1');
+  } else if(isScroll === 1 && $(this).scrollTop() < targetScroll) {
+    isScroll = 0;
+    $('.photo-main').removeClass('photo-position-to')
+    $('.photo-main').addClass('photo-position-from')
+    console.info('change 0');
+  }
+});
