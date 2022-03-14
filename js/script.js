7d52a9fb121a5dd6ac6ac0da841b79ff29ccdf8e
Савенок Дмитрий <36520766+DmitriySavenok@users.noreply.github.com>
2022-03-14 20:56:44 +0300

    tablet styles

--- js/script.js
+++ js/script.js
@@ -1,19 +1,21 @@
-// let isScroll = 0, // доп. проверка
-//     targetScroll = 10; // расстояние до действия / в px
+let isScroll = 0, // доп. проверка
+    targetScroll = 10; // расстояние до действия / в px
 
-// $(window).on('scroll', function(){
-//   if(isScroll === 0 && $(this).scrollTop() >= targetScroll) {
-//     isScroll = 1;
-//     $('.photo-main').removeClass('photo-position-from')
-//     $('.photo-main').addClass('photo-position-to');
-//     console.info('change 1');
-//   } else if(isScroll === 1 && $(this).scrollTop() < targetScroll) {
-//     isScroll = 0;
-//     $('.photo-main').removeClass('photo-position-to')
-//     $('.photo-main').addClass('photo-position-from')
-//     console.info('change 0');
-//   }
-// });
+var $width = $(window).width();
+
+$(window).on('scroll', function(){
+  if ($width > 760) {
+    if(isScroll === 0 && $(this).scrollTop() >= targetScroll) {
+      isScroll = 1;
+      $('.navigation').addClass('navigation-tablet-background');
+      console.info('change 1');
+    } else if(isScroll === 1 && $(this).scrollTop() < targetScroll) {
+      isScroll = 0;
+      $('.navigation').removeClass('navigation-tablet-background')
+      console.info('change 0');
+    }
+  }
+});
 
 // Горизонтальное смещение
 $.fn.moveIt = function(){
