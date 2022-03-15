3bfc2acacd398b0df65b7299941e25128fddfaf7
Савенок Дмитрий <36520766+DmitriySavenok@users.noreply.github.com>
2022-03-15 22:17:47 +0300

    script move photo

--- js/script.js
+++ js/script.js
@@ -1,29 +1,42 @@
-// Горизонтальное смещение фотографии на главном экране
-$.fn.moveIt = function(){
-	var $window = $(window);
-	var instances = [];
+// // Горизонтальное смещение фотографии на главном экране
+// $.fn.moveIt = function(){
+// 	var $window = $(window);
+// 	var instances = [];
 
-	$(this).each(function(){
-		instances.push(new moveItItem($(this)));
-	});
+// 	$(this).each(function(){
+// 		instances.push(new moveItItem($(this)));
+// 	});
 
-	window.addEventListener('scroll', function(){
-		var scrollTop = $window.scrollTop();
-		instances.forEach(function(inst){
-			inst.update(scrollTop) + 550;
-		});
-	}, {passive: true});
-}
+// 	window.addEventListener('scroll', function(){
+// 		var scrollTop = $window.scrollTop();
+// 		instances.forEach(function(inst){
+// 			inst.update(scrollTop) + 550;
+// 		});
+// 	}, {passive: true});
+// }
 
-var moveItItem = function(el){
-	this.el = $(el);
-	this.speed = parseInt(this.el.attr('data-scroll-speed'));
-};
+// var moveItItem = function(el){
+// 	this.el = $(el);
+// 	this.speed = parseInt(this.el.attr('data-scroll-speed'));
+// };
 
-moveItItem.prototype.update = function(scrollTop){
-	this.el.css('transform', 'translateX(' + -(scrollTop / this.speed) + 'px)');
-};
+// moveItItem.prototype.update = function(scrollTop){
+// 	this.el.css('transform', 'translateX(' + -(scrollTop / this.speed) + 'px)');
+// };
 
-$(function(){
-	$('[data-scroll-speed]').moveIt();
+// $(function(){
+// 	$('[data-scroll-speed]').moveIt();
+// });
+
+let isScroll2 = 0, // доп. проверка
+    targetScroll2 = 10; // расстояние до действия / в px
+
+$(window).on('scroll', function(){
+    if(isScroll2 === 0 && $(this).scrollTop() >= targetScroll2) {
+      isScroll2 = 1;
+      $('.photo-main').addClass('photo-translate');
+    } else if(isScroll2 === 1 && $(this).scrollTop() < targetScroll2) {
+      isScroll2 = 0;
+      $('.photo-main').removeClass('photo-translate')
+    }
 });
