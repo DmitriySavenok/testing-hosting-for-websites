11370f5bc2c2220e3a0ca9a1ad3d1c688502a947
Савенок Дмитрий <36520766+DmitriySavenok@users.noreply.github.com>
2022-03-15 19:22:51 +0300

    navigation

--- js/navigation.js
+++ js/navigation.js
@@ -7,20 +7,6 @@ function navigationToggle() {
     logo.classList.toggle("logo-fixed")
 }
 
-$(document).ready(function() {
-  jQuery(".scrollto").click(function () {
-  elementClick = jQuery(this).attr("href")
-
-  $('#navigation').removeClass('navigation__opened')
-  $('#logo').removeClass('logo-fixed')
-
-
-  destination = jQuery(elementClick).offset().top - 70;
-  jQuery("html:not(:animated),body:not(:animated)").animate({scrollTop: destination}, 1000);
-  return false;
-  });
-});
-
 // Добавление класса со стилями для подложки навигации
 let isScroll = 0, // доп. проверка
     targetScroll = 10; // расстояние до действия / в px
@@ -37,4 +23,83 @@ $(window).on('scroll', function(){
       console.info('change 0');
     }
   }
+});
+
+// Смещение навигации при выбора последней секции на ПК версии
+$(document).ready(function() {
+  jQuery(".desktop-contact").click(function () {
+    $('.navigation__block').removeClass('navigation-to-bottom');
+    if($(window).width() > 1400) {
+      $('.navigation__block').addClass('navigation-to-bottom');
+    }
+  });
+});
+
+$(document).ready(function() {
+  jQuery(".desktop-tariffs").click(function () {
+    $('.navigation__block').removeClass('navigation-to-bottom');
+    if($(window).width() > 1400) {
+      if($(".navigation__block").hasClass("navigation-to-bottom"));{
+        $('.navigation__block').removeClass('navigation-to-bottom');
+      }
+    }
+  });
+});
+
+// Смещение навигации при скроле до последнего элемента.
+$(document).ready(function() {
+  if ($(window).width() > 1400) {
+    $(document).ready(function(){
+      var $element = $('.contacts');
+      let counter = 0;
+      $(window).scroll(function() {
+        var scroll = $(window).scrollTop() + $(window).height();
+        //Если скролл до конца елемента
+        var offset = $element.offset().top + $element.height();
+        //Если скролл до начала елемента
+        //var offset = $element.offset().top
+
+        if (scroll > offset && counter == 0) {
+          $('.navigation__block').addClass('navigation-to-bottom');
+          counter = 0;
+        }
+      });
+    });
+  }
+});
+
+$(window).on('scroll', function(){
+  $('.navigation__block').removeClass('navigation-to-bottom');
+  let isScroll1 = 0, // доп. проверка
+    targetScroll1 = 10; // расстояние до действия / в px
+  if ($(window).width() > 1400) {
+    if(isScroll1 === 0 && $(this).scrollTop() <= targetScroll1) {
+      isScroll1 = 1;
+      if($(".navigation__block").hasClass("navigation-to-bottom"));{
+        $('.navigation__block').removeClass('navigation-to-bottom');
+      }
+    } else if(isScroll1 === 1 && $(this).scrollTop() > targetScroll1) {
+      isScroll1 = 0;
+      $('.navigation__block').addClass('navigation-to-bottom');
+    }
+  }
+});
+
+// Скролинг до нужного раздела сайта при нажатии
+$(document).ready(function() {
+  jQuery(".scrollto").click(function () {
+  elementClick = jQuery(this).attr("href")
+
+  $('.navigation__block').removeClass('navigation-to-bottom');
+  $('#navigation').removeClass('navigation__opened')
+  $('#logo').removeClass('logo-fixed')
+  if($(".navigation__block").hasClass("navigation-to-bottom"));{
+    $('.navigation__block').removeClass('navigation-to-bottom');
+  }
+
+
+  destination = jQuery(elementClick).offset().top - 70;
+  jQuery("html:not(:animated),body:not(:animated)").animate({scrollTop: destination}, 1000);
+  return false;
+  });
 });
