079b4855c4ea54c4fb3235e109e12a41e813b7d1
Савенок Дмитрий <36520766+DmitriySavenok@users.noreply.github.com>
2022-05-10 14:06:02 +0300

    fond site test

---
+++ js/contacts-section.js
@@ -0,0 +1,20 @@
+// Contact us (send message script)
+$(document).ready(function() {
+
+	//E-mail Ajax Send
+	$("form").submit(function() { //Change
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "../mail.php", //Change
+			data: th.serialize()
+		}).done(function() {
+			alert("Ваше сообщение отправлено. Спасибо!");
+			setTimeout(function() {
+				// Done Functions
+				th.trigger("reset");
+			}, 1000);
+		});
+		return false;
+	});
+});
