b92a6e9b00fdcc5d1303b3e8321e088278a08271
Савенок Дмитрий <36520766+DmitriySavenok@users.noreply.github.com>
2023-02-05 20:42:43 +0300

    tariffs crads test

---
+++ js/tariffs-screen.js
@@ -0,0 +1,16 @@
+function unwrapTariffcard() {
+  console.log("Скрипт разворачивания карточки загружен")
+
+  document.querySelectorAll('.tariff-card__toggle-button').forEach(el => {
+    el.addEventListener('click', (e) => {
+
+      if (e.target.id === 'basic-tariff-detailed') {
+        basicTariffcard = document.querySelector('#basic-tariff').classList.toggle('tariff-card__toggle-button--pressed');
+      } else if (e.target.id === 'standart-tariff-detailed') {
+        basicTariffcard = document.querySelector('#standart-tariff').classList.toggle('tariff-card__toggle-button--pressed');
+      }
+    })
+  })
+}
+
+unwrapTariffcard();
