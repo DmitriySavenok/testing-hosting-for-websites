49adfc08f2a42083b265daa7392a4d3553c48903
Савенок Дмитрий <36520766+DmitriySavenok@users.noreply.github.com>
2023-02-23 19:59:00 +0300

    test payment

--- js/reviews-screen.js
+++ js/reviews-screen.js
@@ -1,6 +1,14 @@
 $(document).ready(function(){
   $('.reviews-screen__slider').slick({
     arrows: false,
-    dots: true
+    dots: true,
+    slidesToShow: 2,
+    responsive: [
+      {
+        breakpoint: 1366,
+          settings: {
+          slidesToShow: 1
+        }
+      }]
   })
 })
