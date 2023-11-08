312d08029d8524639ae3e8f9b1080f39018daaa8
Савенок Дмитрий <36520766+DmitriySavenok@users.noreply.github.com>
2023-11-08 15:49:04 +0300

    smm generator fix

--- js/canvas.js
+++ js/canvas.js
@@ -10,46 +10,50 @@ const text = document.getElementById('text_input');
 document.getElementById('photo_input').value = '';
 
 let title = 'Введите текст';
-let title_bgr = ['../src/text_background.jpg', 500, 115];
+let title_bgr = ['./src/text_background.jpg', 500, 115];
 let font_size = 32;
 const font_color = '#E8E8D9';
 
 input.value = font_size;
 text.value = '';
 
-let photo = ['../src/photo_empty_bg.jpg'];
+let photo = ['./src/photo_empty_bg.jpg'];
 
 function drawCanvas() {
-    canvas.width = canvas_width;
-    canvas.height = canvas_height;
 
-    // Загрузка фото
-    let photo_empty_bg = new Image();
-    photo_empty_bg.onload = function () {
-        context.drawImage(photo_empty_bg, 0, 0, 500, 385);
-    };
-    photo_empty_bg.src = photo[0];
+    if (canvas.getContext) {
+        canvas.width = canvas_width;
+        canvas.height = canvas_height;
 
-    // Загрузка фото с устройства
-    photo_input.onchange = function (e) {
-        var URL = window.webkitURL || window.URL;
-        photo[0] = URL.createObjectURL(e.target.files[0]);
-        var img = new Image();
-        img.onload = function () {
-            context.drawImage(img, 0, 0, 500, 385);
+        // Загрузка фото
+        let photo_empty_bg = new Image();
+        photo_empty_bg.onload = function () {
+            context.drawImage(photo_empty_bg, 0, 0, 500, 385);
+        };
+        photo_empty_bg.src = photo[0];
+
+        // Загрузка фото с устройства
+        photo_input.onchange = function (e) {
+            var URL = window.webkitURL || window.URL;
+            photo[0] = URL.createObjectURL(e.target.files[0]);
+            var img = new Image();
+            img.onload = function () {
+                context.drawImage(img, 0, 0, 500, 385);
+            }
+            img.src = photo[0];
         }
-        img.src = photo[0];
+
+        // Плашка под заголовок
+        let titleBackground = new Image();
+        titleBackground.onload = function () {
+            context.drawImage(titleBackground, 0, 385, title_bgr[1], title_bgr[2]);
+            context.font = font_size + "px sans-serif";
+            context.fillStyle = font_color;
+            context.fillText(title, 165, canvas_height - 50);
+        };
+        titleBackground.src = title_bgr[0];
     }
 
-    // Плашка под заголовок
-    let titleBackground = new Image();
-    titleBackground.onload = function () {
-        context.drawImage(titleBackground, 0, 385, title_bgr[1], title_bgr[2]);
-        context.font = font_size + "px sans-serif";
-        context.fillStyle = font_color;
-        context.fillText(title, 165, canvas_height - 50);
-    };
-    titleBackground.src = title_bgr[0];
 }
 
 drawCanvas();
